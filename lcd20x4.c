49cf9cfd179a491fc16a87f6967a4f261557c151
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-08-03 10:03:26 +0700

    Create lcd20x4.c

---
+++ lcd20x4.c
@@ -0,0 +1,94 @@
+/Author: Controller Tech + Me */
+/** Put this in the src folder **/
+
+#include "i2c-lcd.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "unistd.h"
+
+#define SLAVE_ADDRESS_LCD 0x4E>>1 // change this according to ur setup
+
+esp_err_t err;
+
+#define I2C_NUM I2C_NUM_1
+
+static const char *TAG = "LCD";
+
+int address[4][20] = {
+  {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13},
+  {0x44, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53},
+  {0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27},
+  {0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67}
+};
+
+void lcd_send_cmd (char cmd)
+{
+  char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (cmd&0xf0);
+	data_l = ((cmd<<4)&0xf0);
+	data_t[0] = data_u|0x0C;  //en=1, rs=0
+	data_t[1] = data_u|0x08;  //en=0, rs=0
+	data_t[2] = data_l|0x0C;  //en=1, rs=0
+	data_t[3] = data_l|0x08;  //en=0, rs=0
+	err = i2c_master_write_to_device(I2C_NUM, SLAVE_ADDRESS_LCD, data_t, 4, 1000);
+	if (err!=0) ESP_LOGI(TAG, "Error in sending command");
+}
+
+void lcd_send_data (char data)
+{
+	char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (data&0xf0);
+	data_l = ((data<<4)&0xf0);
+	data_t[0] = data_u|0x0D;  //en=1, rs=0
+	data_t[1] = data_u|0x09;  //en=0, rs=0
+	data_t[2] = data_l|0x0D;  //en=1, rs=0
+	data_t[3] = data_l|0x09;  //en=0, rs=0
+	err = i2c_master_write_to_device(I2C_NUM, SLAVE_ADDRESS_LCD, data_t, 4, 1000);
+	if (err!=0) ESP_LOGI(TAG, "Error in sending data");
+}
+
+void lcd_clear (void)
+{
+	lcd_send_cmd (0x01);
+	usleep(5000);
+}
+
+void lcd_put_cur(int row, int col)
+{
+	lcd_send_cmd (0x80|address[row][col]);
+}
+
+
+void lcd_init (void)
+{
+	// 4 bit initialisation
+	usleep(50000);  // wait for >40ms
+	lcd_send_cmd (0x30);
+	usleep(5000);  // wait for >4.1ms
+	lcd_send_cmd (0x30);
+	usleep(200);  // wait for >100us
+	lcd_send_cmd (0x30);
+	usleep(10000);
+	lcd_send_cmd (0x20);  // 4bit mode
+	usleep(10000);
+
+  // dislay initialisation
+	lcd_send_cmd (0x28); // Function set --> DL=0 (4 bit mode), N = 1 (2 line display) F = 0 (5x8 characters)
+	usleep(1000);
+	lcd_send_cmd (0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
+	usleep(1000);
+	lcd_send_cmd (0x01);  // clear display
+	usleep(1000);
+	usleep(1000);
+	lcd_send_cmd (0x06); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
+	usleep(1000);
+	lcd_send_cmd (0x0C); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
+	usleep(1000);
+}
+
+void lcd_send_string (char *str)
+{
+	while (*str) lcd_send_data (*str++);
+}
